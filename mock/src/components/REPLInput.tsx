f885e833bff2873055fb0f8abb23b2ba31ad998f
Elijah <ewhang3@gmail.com>
2023-10-07 17:26:30 -0400

    feat: worked on user story 1 and started working on user story 2 with @ewang111

--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -3,26 +3,47 @@ import { Dispatch, SetStateAction, useState} from 'react';
 import { ControlledInput } from './ControlledInput';
 
 interface REPLInputProps{
-  // TODO: Fill this with desired props... Maybe something to keep track of the submitted commands
   history: string[],
   setHistory: Dispatch<SetStateAction<string[]>>
 }
 // You can use a custom interface or explicit fields or both! An alternative to the current function header might be:
 // REPLInput(history: string[], setHistory: Dispatch<SetStateAction<string[]>>)
 export function REPLInput(props : REPLInputProps) {
-    // Remember: let React manage state in your webapp. 
-    // Manages the contents of the input box
+
     const [commandString, setCommandString] = useState<string>('');
-    // TODO WITH TA : add a count state
     const [count, setCount] = useState(0);
+    const [mode, setMode] = useState("brief")
+    const [action, setAction] = useState("")
     
-    // TODO WITH TA: build a handleSubmit function called in button onClick
-    // TODO: Once it increments, try to make it push commands... Note that you can use the `...` spread syntax to copy what was there before
-    // add to it with new commands.
     function handleSubmit(commandString: string) {
       setCount(count + 1)
-      props.setHistory([...props.history, commandString])
+      if (mode === "brief") {
+        props.setHistory([...props.history, evaluteCommand(commandString)])
+      }
+      if (mode === "verbose") {
+        var command = "Command: " + commandString
+        var output = "Output: " + evaluteCommand(commandString)  
+        props.setHistory([...props.history, command, output])
+      }
       setCommandString('')
+      if (action === "load_file") {
+        
+      }
+    }
+
+    function evaluteCommand(commandString: string): string {
+      if (commandString === "brief mode") {
+        setMode("brief")
+        return "mode set to brief"
+      } else if (commandString === "verbose mode") {
+        setMode("verbose")
+        return "mode set to verbose"
+      } else if (commandString.substring(0, 8) === ("load_file")) {
+        setAction("load_file")
+        return "loading file"
+      } else {
+        return "unknown command"
+      }
     }
     /**
      * We suggest breaking down this component into smaller components, think about the individual pieces 
@@ -40,8 +61,7 @@ export function REPLInput(props : REPLInputProps) {
             </fieldset>
             {/* TODO WITH TA: Build a handleSubmit function that increments count and displays the text in the button */}
             {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
-            <button onClick= {() => handleSubmit(commandString)}>Submit</button>
-            <text>Pressed {count} times</text>
+            <button onClick={() => handleSubmit(commandString)}>Submit</button>
         </div>
     );
   }
