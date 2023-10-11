bfcf56d9b9a837c8e3e1f921e67f2e1b83d99a1d
Elijah <ewhang3@gmail.com>
2023-10-11 00:24:28 -0400

    feat: formatting view into tables and also changing verbose spacing

--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -7,8 +7,8 @@ import BasicNoHeaderCSV from '../data/BasicNoHeaderCSV';
 import EmptyCSV from '../data/EmptyCSV';
 
 interface REPLInputProps{
-  history: string[],
-  setHistory: Dispatch<SetStateAction<string[]>>
+  history: string[][],
+  setHistory: Dispatch<SetStateAction<string[][]>>
 }
 // You can use a custom interface or explicit fields or both! An alternative to the current function header might be:
 // REPLInput(history: string[], setHistory: Dispatch<SetStateAction<string[]>>)
@@ -16,40 +16,33 @@ export function REPLInput(props : REPLInputProps) {
 
     const [commandString, setCommandString] = useState<string>('')
     const [mode, setMode] = useState("brief")
-    const [action, setAction] = useState("")
     const [csv, setCSV] = useState<Array<Array<string>>>()
+    const [action, setAction] = useState<string>('')
+    const [column, setColumn] = useState("")
+    const [value, setValue] = useState("")
+    const [header, setHeader] = useState("")
+    const [searcher, setSearcher] = useState<Array<String>>()
+    const [result, setResult] = useState<Array<Array<string>>>()
 
-    const fileMap = new Map<string, Array<Array<string>>>();
+    const fileMap = new Map<string, Array<Array<string>>>()
     fileMap.set("mock/src/data/BadCSV.ts", BadCSV)
     fileMap.set("mock/src/data/BasicHeaderCSV.ts", BasicHeaderCSV)
     fileMap.set("mock/src/data/BasicNoHeaderCSV.ts", BasicNoHeaderCSV)
     fileMap.set("mock/src/data/EmptyCSV.ts", EmptyCSV)
     
     function handleMode(commandString: string) {
+      let output = "Output: " + evaluateCommand(commandString)
       if (mode === "brief") {
-        //handleAction(commandString)
-        props.setHistory([...props.history, evaluteCommand(commandString)])
+        props.setHistory([...props.history, ["brief", commandString, output]])
       }
       if (mode === "verbose") {
-        var command = "Command: " + commandString + ", "
-        var output = "Output: " + evaluteCommand(commandString)
-        // TODO: must be on separate lines !! 
-        //handleAction(commandString)
-        props.setHistory([...props.history, command +  <br /> + output])
+        var command = "Command: " + commandString
+        props.setHistory([...props.history, [command, output]])
       }
       setCommandString("");
     }
 
-    // function handleAction(commandString: string){
-    //   if (action === "load_file") {
-    //     props.setHistory([...props.history, evaluteCommand(commandString)]);
-    //   }
-    //   if (action === "view_file") {
-    //     props.setHistory([...props.history, evaluteCommand(commandString)]);
-    //   }
-    // }
-
-    function evaluteCommand(commandString: string): string {
+    function evaluateCommand(commandString: string): string {
       if (commandString === "mode brief") {
         setMode("brief")
         return "mode set to brief"
@@ -57,7 +50,6 @@ export function REPLInput(props : REPLInputProps) {
         setMode("verbose")
         return "mode set to verbose"
       } else if (commandString.substring(0, 9) === ("load_file")) {
-        setAction("load_file")
         var file_path = commandString.substring(10, commandString.length)
         if (fileMap.has(file_path)){
           setCSV(fileMap.get(file_path));
@@ -65,32 +57,33 @@ export function REPLInput(props : REPLInputProps) {
         }
         return "failure to load file: " + file_path
       } else if (commandString === ("view")) {
-        setAction("view")
-        if (csv){
-          // TODO: csv must be in a table format
-          return "file: " + csv 
+        if (csv) {
+          return JSON.stringify(csv)
         }
         return "failure to view file"
+      } else if (commandString.substring(0, 6) === ("search column =")){
+        var input = commandString.split("=")
+        setColumn(input[1].substring(0, input[1].length - 6))
+        if (input[1].substring(input[1].length - 6,) === "value "){
+          setValue(input[2].substring(0, input[2].length - 7))
+          if (input[2].substring(input[2].length - 6,) === "header "){
+            setHeader(input[3])
+            setSearcher([column, value, header])
+            return "" + searcher
+          }
+        }
+        return "failure to search" // change this
       } else {
         return "unknown command"
       }
     }
-    /**
-     * We suggest breaking down this component into smaller components, think about the individual pieces 
-     * of the REPL and how they connect to each other...
-     */
+    
     return (
         <div className="repl-input">
-            {/* This is a comment within the JSX. Notice that it's a TypeScript comment wrapped in
-            braces, so that React knows it should be interpreted as TypeScript */}
-            {/* I opted to use this HTML tag; you don't need to. It structures multiple input fields
-            into a single unit, which makes it easier for screenreaders to navigate. */}
             <fieldset>
               <legend>Enter a command:</legend>
               <ControlledInput value={commandString} setValue={setCommandString} ariaLabel={"Command input"}/>
             </fieldset>
-            {/* TODO WITH TA: Build a handleSubmit function that increments count and displays the text in the button */}
-            {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
             <button onClick={() => handleMode(commandString)}>Submit</button>
         </div>
     );
